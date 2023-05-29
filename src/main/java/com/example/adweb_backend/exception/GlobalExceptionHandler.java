a80ce5a894a7a0ab38bf0adef344906291592ad5
xintianle <20307110196@fudan.edu.cn>
2023-05-29 12:51:59 +0800

    增加缺少请求头的异常处理

--- src/main/java/com/example/adweb_backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/adweb_backend/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -23,5 +24,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NumberFormatException.class)
     public ResponseEntity<Object> numberFormatError(NumberFormatException e){
         return new ResponseEntity<Object>(new MessageResponse("数字转换错误"), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<Object> missReqHeader(MissingRequestHeaderException e){
+        if (e.getHeaderName().equals("Authorization"))
+            return new ResponseEntity<Object>(new MessageResponse(e.getMessage()), HttpStatus.UNAUTHORIZED);
+
+        return new ResponseEntity<Object>(new MessageResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 }
