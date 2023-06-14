c1df601bf9fae380d87b83d899d62274664c306e
xintianle <20307110196@fudan.edu.cn>
2023-06-14 23:20:21 +0800

    增加定时、查询、锁

--- src/main/java/com/example/adweb_backend/AdwebBackendApplication.java
+++ src/main/java/com/example/adweb_backend/AdwebBackendApplication.java
@@ -2,7 +2,9 @@ package com.example.adweb_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling //开启定时功能的注解
 @SpringBootApplication
 public class AdwebBackendApplication {
 
--- src/main/java/com/example/adweb_backend/controller/DistributionController.java
+++ src/main/java/com/example/adweb_backend/controller/DistributionController.java
@@ -1,8 +1,13 @@
 package com.example.adweb_backend.controller;
 
 import com.example.adweb_backend.util.JWTToken;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -13,8 +18,29 @@ import java.util.concurrent.locks.ReentrantLock;
 public class DistributionController {
 
     public static Lock lock = new ReentrantLock();
+    public static Lock gLock = new ReentrantLock();
+    public static boolean genOK = true;
 
     public static final int WIDTH = 40;
+
+    public static short[][] last_result;
+    public static long last = 0;
+
+    @Scheduled(cron = "0 * * * * ?")
+    private static void makeGenOK(){
+        if(gLock.tryLock()){
+            try {
+                genOK = true;
+            }
+            catch (Exception e){
+            }
+            finally {
+                gLock.unlock();
+            }
+        }
+        else{
+        }
+    }
 
     public static short[][] generateXYNormal(double miu, double sigma, int times){
         Random r = new Random();
@@ -42,7 +68,6 @@ public class DistributionController {
         return result;
     }
 
-    @RequestMapping(value = "/lock", method = RequestMethod.GET)
     public void concurrentDo(){
         if(lock.tryLock()){
             try {
@@ -65,30 +90,96 @@ public class DistributionController {
 
 
     @RequestMapping(value = "/distribution/normalXY", method = RequestMethod.GET)
-    public Object getNormalXYDistribution(@RequestHeader(name = "Authorization") String token,
+    public Object getNormalXYDistribution(
+//            @RequestHeader(name = "Authorization") String token,
                                   @RequestParam(name = "miu") double miu,
                                   @RequestParam(name = "sigma") double sigma,
                                   @RequestParam(name = "times") int times
-                                  ){
-        int tid = JWTToken.verify(token).getClaims().get("id").asInt();
+                                  ) {
+        if (gLock.tryLock()) {
+            try {
+                System.out.println(Thread.currentThread().getName() + ": get the lock!");
+                if (genOK) {
+                    genOK = false;
+//                    int tid = JWTToken.verify(token).getClaims().get("id").asInt();
 
-        if (times > 10000)
-            times = 10000;
-        return generateXYNormal(miu, sigma, times);
+                    if (times > 10000)
+                        times = 10000;
+                    last_result = generateXYNormal(miu, sigma, times);
+                    last+=1;
+                    return last_result;
+                } else {
+                    System.out.println(Thread.currentThread().getName() + ": but gen not ok...");
+                    return new ResponseEntity<Object>(null, HttpStatus.CONFLICT);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(Thread.currentThread().getName() + ": release the lock!");
+                gLock.unlock();
+            }
+        } else {
+            System.out.println(Thread.currentThread().getName() + ": can't get the lock...");
+            return new ResponseEntity<Object>(null, HttpStatus.CONFLICT);
+        }
+
+        return null;
     }
 
 
     @RequestMapping(value = "/distribution/uniformXY", method = RequestMethod.GET)
-    public Object getUniformXYDistribution(@RequestHeader(name = "Authorization") String token,
+    public Object getUniformXYDistribution(
+//            @RequestHeader(name = "Authorization") String token,
                                           @RequestParam(name = "times") int times
     ){
-        int tid = JWTToken.verify(token).getClaims().get("id").asInt();
+        if (gLock.tryLock()) {
+            try {
+                System.out.println(Thread.currentThread().getName() + ": get the lock!");
+                if (genOK) {
+                    genOK = false;
 
-        if (times > 10000)
-            times = 10000;
-        return generateXYUniform(times);
+//                    int tid = JWTToken.verify(token).getClaims().get("id").asInt();
+
+                    if (times > 10000)
+                        times = 10000;
+                    last_result = generateXYUniform(times);
+                    last+=1;
+                    return last_result;
+
+                } else {
+                    System.out.println(Thread.currentThread().getName() + ": but gen not ok...");
+                    return new ResponseEntity<Object>(null, HttpStatus.CONFLICT);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(Thread.currentThread().getName() + ": release the lock!");
+                gLock.unlock();
+            }
+        } else {
+            System.out.println(Thread.currentThread().getName() + ": can't get the lock...");
+            return new ResponseEntity<Object>(null, HttpStatus.CONFLICT);
+        }
+
+        return null;
     }
 
+    @RequestMapping(value = "/distribution/check", method = RequestMethod.GET)
+    public Object checkLastResult(
+            @RequestParam(name = "now") int now){
+//        System.out.println(last);
+//        System.out.println(now);
+        if (now == last){
+            return new ResponseEntity<Object>(null, HttpStatus.NOT_MODIFIED);
+        }
+        else{
+            Map<String, Object> map = new HashMap<>();
+            map.put("now", last);
+            map.put("data", last_result);
+            return new ResponseEntity<Object>(map, HttpStatus.OK);
+        }
+
+    }
 
 
 }
